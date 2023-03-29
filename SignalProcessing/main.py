6530ff2109a5e8a4329659ccc4ad83913f9a58f0
Sondre Olimb <sondre.olimb@gmail.com>
2023-03-29 15:57:06 +0100

    feat: SP finished

--- SignalProcessing/main.py
+++ SignalProcessing/main.py
@@ -6,21 +6,22 @@ from .DopplerProcessing import DopplerProcessing
 from .CFAR import CFAR_1D
 
 def SignalProcesingAlgorithem(data,argArtifacts):
+    print(data.shape)
     range_cube =  RangeCompression(data,axis=1)
-    linear, mag = DopplerProcessing(range_cube, axis=0,isClutterRemoval=True)
+    linear= DopplerProcessing(range_cube, axis=0,isClutterRemoval=True)
     linear[argArtifacts] = 1e-10
     detections_map,P_detections,detections, detections_cord,det_tuples = CFAR_1D(np.abs(linear).copy(), 4, 8, 0.01)
     return detections_cord , detections_map
 
 def Artifacts(data):
-    print("art")
+   
     range_cube =  RangeCompression(data,axis=1)
-    print("art 1")
+   
     linear = DopplerProcessing(range_cube, axis=0,isClutterRemoval=True,removeArtifacts=False)
-    print("art 2")
+    
     old = np.abs(linear).copy()
-    print("art 3")
+   
     argArtifacts = np.nonzero(old >4*np.mean(old[:,50]))
-    print("art done")
+   
     return argArtifacts
 
--- SignalProcessingProcess.py
+++ SignalProcessingProcess.py
@@ -10,13 +10,13 @@ def SPP(data_queue,detection_queue):
     while True:
         data = data_queue.get()
         if data is not None:
-            print("started SP")
+            
             if artifacts is None:
                 artifacts = Artifacts(data)
             
             else:
-                detection_queue.put( SignalProcesingAlgorithem(data[0],artifacts))
-            print("finished SP")
+                detection_queue.put( SignalProcesingAlgorithem(data,artifacts))
+           
 
    
 
